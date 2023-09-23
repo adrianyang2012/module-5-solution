d4b63ed5684760328c09eab18097639dc924b127
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-09-23 13:56:46 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -121,7 +121,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#container",homeHtmlToInsertIntoMainPage);
+      insertHtml('#home-tiles',homeHtmlToInsertIntoMainPage);
       
 
     },
