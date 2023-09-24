8bbbdf287c76332892107df3a66148bc5f0faa15
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-09-24 16:19:31 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,10 +82,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // *** start ***
 // On first load, show home view
 showLoading("#main-content");
-alert(allCategoriesUrl);
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [buildAndShowHomeHTML(allCategoriesUrl['short_name'])], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  [buildAndShowHomeHTML], // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
